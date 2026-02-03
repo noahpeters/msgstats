8bccd9fffc6ef1b7d83c04c1fdb914248fa25d40
Noah Peters <noahpeters@gmail.com>
2026-02-03 13:18:23 -0800

    rebased

--- scripts/pre-push.mjs
+++ scripts/pre-push.mjs
@@ -4,10 +4,5 @@ const run = (command) => {
   execSync(command, { stdio: 'inherit' });
 };
 
-const branch = execSync('git rev-parse --abbrev-ref HEAD', {
-  encoding: 'utf8',
-}).trim();
-
 run('npm run verify');
-run('npm run check:remote-migrations:staging');
-run('npm run check:remote-migrations:prod');
+run('npm run check:migration-edits');
